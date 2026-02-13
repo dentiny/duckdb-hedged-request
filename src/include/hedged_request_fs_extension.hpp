51c1153ca84ac6e3331f9d12e9096d6119c1779e
dentiny <dentinyhao@gmail.com>
2026-02-13 00:08:36 -0800

    Rename extension (#18)

--- src/include/hedged_request_fs_extension.hpp
+++ src/include/hedged_request_fs_extension.hpp
@@ -4,7 +4,7 @@
 
 namespace duckdb {
 
-class QuackExtension : public Extension {
+class HedgedRequestFsExtension : public Extension {
 public:
 	void Load(ExtensionLoader &db) override;
 	std::string Name() override;
