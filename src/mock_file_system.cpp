e98eb69ed95b18e16cf741109412bf9d6733cbae
dentiny <dentinyhao@gmail.com>
2026-02-13 22:32:55 -0800

    [minor] Use atomic for mock fs (#25)

--- src/mock_file_system.cpp
+++ src/mock_file_system.cpp
@@ -16,83 +16,67 @@ void MockFileSystem::SetDelay(std::chrono::milliseconds delay_p) {
 }
 
 idx_t MockFileSystem::GetOpenFileCount() const {
-	const lock_guard<mutex> lock(count_mutex);
-	return open_file_count;
+	return open_file_count.load();
 }
 
 void MockFileSystem::ResetOpenFileCount() {
-	const lock_guard<mutex> lock(count_mutex);
-	open_file_count = 0;
+	open_file_count.store(0);
 }
 
 idx_t MockFileSystem::GetFileExistsCount() const {
-	const lock_guard<mutex> lock(count_mutex);
-	return file_exists_count;
+	return file_exists_count.load();
 }
 
 void MockFileSystem::ResetFileExistsCount() {
-	const lock_guard<mutex> lock(count_mutex);
-	file_exists_count = 0;
+	file_exists_count.store(0);
 }
 
 idx_t MockFileSystem::GetDirectoryExistsCount() const {
-	const lock_guard<mutex> lock(count_mutex);
-	return directory_exists_count;
+	return directory_exists_count.load();
 }
 
 void MockFileSystem::ResetDirectoryExistsCount() {
-	const lock_guard<mutex> lock(count_mutex);
-	directory_exists_count = 0;
+	directory_exists_count.store(0);
 }
 
 idx_t MockFileSystem::GetListFilesCount() const {
-	const lock_guard<mutex> lock(count_mutex);
-	return list_files_count;
+	return list_files_count.load();
 }
 
 void MockFileSystem::ResetListFilesCount() {
-	const lock_guard<mutex> lock(count_mutex);
-	list_files_count = 0;
+	list_files_count.store(0);
 }
 
 idx_t MockFileSystem::GetGlobCount() const {
-	const lock_guard<mutex> lock(count_mutex);
-	return glob_count;
+	return glob_count.load();
 }
 
 void MockFileSystem::ResetGlobCount() {
-	const lock_guard<mutex> lock(count_mutex);
-	glob_count = 0;
+	glob_count.store(0);
 }
 
 idx_t MockFileSystem::GetFileSizeCount() const {
-	const lock_guard<mutex> lock(count_mutex);
-	return get_file_size_count;
+	return get_file_size_count.load();
 }
 
 void MockFileSystem::ResetFileSizeCount() {
-	const lock_guard<mutex> lock(count_mutex);
-	get_file_size_count = 0;
+	get_file_size_count.store(0);
 }
 
 idx_t MockFileSystem::GetLastModifiedTimeCount() const {
-	const lock_guard<mutex> lock(count_mutex);
-	return get_last_modified_time_count;
+	return get_last_modified_time_count.load();
 }
 
 void MockFileSystem::ResetLastModifiedTimeCount() {
-	const lock_guard<mutex> lock(count_mutex);
-	get_last_modified_time_count = 0;
+	get_last_modified_time_count.store(0);
 }
 
 idx_t MockFileSystem::GetFileTypeCount() const {
-	const lock_guard<mutex> lock(count_mutex);
-	return get_file_type_count;
+	return get_file_type_count.load();
 }
 
 void MockFileSystem::ResetFileTypeCount() {
-	const lock_guard<mutex> lock(count_mutex);
-	get_file_type_count = 0;
+	get_file_type_count.store(0);
 }
 
 string MockFileSystem::GetName() const {
@@ -101,28 +85,19 @@ string MockFileSystem::GetName() const {
 
 unique_ptr<FileHandle> MockFileSystem::OpenFile(const string &path, FileOpenFlags flags,
                                                 optional_ptr<FileOpener> opener) {
-	{
-		const lock_guard<mutex> lock(count_mutex);
-		open_file_count++;
-	}
+	open_file_count++;
 	SimulateDelay();
 	return LocalFileSystem::OpenFile(path, flags, opener);
 }
 
 int64_t MockFileSystem::GetFileSize(FileHandle &handle) {
-	{
-		const lock_guard<mutex> lock(count_mutex);
-		get_file_size_count++;
-	}
+	get_file_size_count++;
 	SimulateDelay();
 	return LocalFileSystem::GetFileSize(handle);
 }
 
 timestamp_t MockFileSystem::GetLastModifiedTime(FileHandle &handle) {
-	{
-		const lock_guard<mutex> lock(count_mutex);
-		get_last_modified_time_count++;
-	}
+	get_last_modified_time_count++;
 	SimulateDelay();
 	return LocalFileSystem::GetLastModifiedTime(handle);
 }
@@ -133,57 +108,39 @@ string MockFileSystem::GetVersionTag(FileHandle &handle) {
 }
 
 FileType MockFileSystem::GetFileType(FileHandle &handle) {
-	{
-		const lock_guard<mutex> lock(count_mutex);
-		get_file_type_count++;
-	}
+	get_file_type_count++;
 	SimulateDelay();
 	return LocalFileSystem::GetFileType(handle);
 }
 
 bool MockFileSystem::DirectoryExists(const string &directory, optional_ptr<FileOpener> opener) {
-	{
-		const lock_guard<mutex> lock(count_mutex);
-		directory_exists_count++;
-	}
+	directory_exists_count++;
 	SimulateDelay();
 	return LocalFileSystem::DirectoryExists(directory, opener);
 }
 
 bool MockFileSystem::FileExists(const string &filename, optional_ptr<FileOpener> opener) {
-	{
-		const lock_guard<mutex> lock(count_mutex);
-		file_exists_count++;
-	}
+	file_exists_count++;
 	SimulateDelay();
 	return LocalFileSystem::FileExists(filename, opener);
 }
 
 vector<OpenFileInfo> MockFileSystem::Glob(const string &path, FileOpener *opener) {
-	{
-		const lock_guard<mutex> lock(count_mutex);
-		glob_count++;
-	}
+	glob_count++;
 	SimulateDelay();
 	return LocalFileSystem::Glob(path, opener);
 }
 
 bool MockFileSystem::ListFiles(const string &directory, const std::function<void(const string &, bool)> &callback,
                                FileOpener *opener) {
-	{
-		const lock_guard<mutex> lock(count_mutex);
-		list_files_count++;
-	}
+	list_files_count++;
 	SimulateDelay();
 	return LocalFileSystem::ListFiles(directory, callback, opener);
 }
 
 unique_ptr<FileHandle> MockFileSystem::OpenFileExtended(const OpenFileInfo &info, FileOpenFlags flags,
                                                         optional_ptr<FileOpener> opener) {
-	{
-		const lock_guard<mutex> lock(count_mutex);
-		open_file_count++;
-	}
+	open_file_count++;
 	SimulateDelay();
 	return LocalFileSystem::OpenFile(info.path, flags, opener);
 }
